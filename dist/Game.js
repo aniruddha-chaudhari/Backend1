1b8a36ed2d5cd6e186fde2162a5bf8ac92ff4831
aniruddha-chaudhari <140920431+aniruddha-chaudhari@users.noreply.github.com>
2024-07-15 21:46:59 +0530

    simple backend

--- dist/Game.js
+++ dist/Game.js
@@ -1,70 +1,59 @@
-import WebSocket from "ws";
-import { Chess } from "chess.js";
-import { GAME_OVER, INIT_GAME, MOVE } from "./messages";
-
-export class Game {
-    public player1: WebSocket;
-    public player2: WebSocket;
-    private board: Chess;
-    private startTime: Date;
-
-    constructor(player1: WebSocket, player2: WebSocket) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Game = void 0;
+const chess_js_1 = require("chess.js");
+const messages_1 = require("./messages");
+class Game {
+    constructor(player1, player2) {
         this.player1 = player1;
         this.player2 = player2;
-        this.board = new Chess();
+        this.board = new chess_js_1.Chess();
         this.startTime = new Date();
-
         this.player1.send(JSON.stringify({
-            type: INIT_GAME,
+            type: messages_1.INIT_GAME,
             payload: {
                 color: "white"
             }
         }));
-
         this.player2.send(JSON.stringify({
-            type: INIT_GAME,
+            type: messages_1.INIT_GAME,
             payload: {
                 color: "black"
             }
         }));
     }
-
-    makeMove(socket: WebSocket, move: string) {
+    makeMove(socket, move) {
         // Check if the move is from the correct player
         if ((this.board.turn() === 'w' && socket !== this.player1) || (this.board.turn() === 'b' && socket !== this.player2)) {
             return;
         }
-
         try {
             this.board.move(move);
-        } catch (error) {
+        }
+        catch (error) {
             console.log(error);
             return;
-            
         }
-
         // Check if the game is over
         if (this.board.isGameOver()) {
             const winner = this.board.turn() === 'w' ? 'Black' : 'White';
             const message = JSON.stringify({
-                type: GAME_OVER,
+                type: messages_1.GAME_OVER,
                 payload: {
                     winner: winner
                 }
             });
-
             this.player1.send(message);
             this.player2.send(message);
             return;
         }
-
         // Notify both players of the move
         const message = JSON.stringify({
-            type: MOVE,
+            type: messages_1.MOVE,
             payload: move
         });
-
         this.player1.send(message);
         this.player2.send(message);
     }
 }
+exports.Game = Game;
